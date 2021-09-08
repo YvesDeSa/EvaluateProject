70b357839336faa0d7e4d923cc273f78242ea2ec
Yves de Sá <yvesdsb98@gmail.com>
2021-09-08 02:05:10 -0300

    added jpa dependency and configuring database access

--- src/main/java/com/evaluate/evaluate/EvaluateApplication.java
+++ src/main/java/com/evaluate/evaluate/EvaluateApplication.java
@@ -7,7 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class EvaluateApplication {
 
 	public static void main(String[] args) {
-            SpringApplication.run(EvaluateApplication.class,args);
+		SpringApplication.run(EvaluateApplication.class, args);
 	}
 
 }
