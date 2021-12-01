f0a6e838a24d3ce5253d3ff96333226cd5b80e6e
Yves de Sá <yvesdsb98@gmail.com>
2021-12-01 17:21:01 -0300

    Updated the repository type of the admin repositories return.

--- src/main/java/com/evaluate/repository/AdminRepository.java
+++ src/main/java/com/evaluate/repository/AdminRepository.java
@@ -1,15 +1,14 @@
 package com.evaluate.repository;
 
 import com.evaluate.model.Admin;
-import com.evaluate.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface AdminRepository  extends JpaRepository<Admin, Long>{
-    public User findByEmail(String email);
+    public Admin findByEmail(String email);
     
-    public User findByToken(Long token);
+    public Admin findByToken(Long token);
     
-    public User findByLogin(String login);
+    public Admin findByLogin(String login);
 }
